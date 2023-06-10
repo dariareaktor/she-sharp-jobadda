e603caeee1f0616dee76205d449718264be908ad
Daria Artemova <119043985+dariareaktor@users.noreply.github.com>
2023-06-10 16:53:35 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-if (environment.production) {
-  enableProdMode();
-}
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
